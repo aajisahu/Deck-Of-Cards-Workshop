6b88f629e5e012c05e7b6787435d207e0dc02ac8
aajisahu <aajisahu.com>
2021-10-29 23:21:12 +0530

    Added: There should be minimum 2 and maximum 4 players in the Game

--- src/main/java/com/bridgelabz/workshop/deckofcards/DeckOfCardsMain.java
+++ src/main/java/com/bridgelabz/workshop/deckofcards/DeckOfCardsMain.java
@@ -1,10 +1,9 @@
 package com.bridgelabz.workshop.deckofcards;
 
-
 public class DeckOfCardsMain {
     public static void main(String[] args) {
         /*
-         * In this I have created an object of DeckOfCardsGame class and just calling the method through object.
+        In this I have created an object of DeckOfCardsGame class and just calling the method through object.
          */
         DeckOfCardsGame deckOfCardsGame = new DeckOfCardsGame();
         deckOfCardsGame.welcome();
